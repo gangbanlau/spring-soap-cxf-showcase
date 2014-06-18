7c03f265998c611423daf7b35a5793f51faa2150
zhangfb512@gmail.com <zhangfb512@gmail.com@701a5e63-cc8d-c32d-bdff-8dc5e51d885c>
2014-06-18 09:37:00 +0000

    初始提交。

---
+++ zhangfb/soap-cxf-test/src/main/java/com/maingel/soap/service/impl/HelloWorldImpl.java
@@ -0,0 +1,13 @@
+package com.maingel.soap.service.impl;
+
+import javax.jws.WebService;
+
+import com.maingel.soap.service.IHelloWorld;
+
+@WebService(endpointInterface = "com.maingel.soap.service.IHelloWorld", serviceName = "HelloWorld")
+public class HelloWorldImpl implements IHelloWorld {
+	public String sayHi(String text) {
+		System.out.println("sayHi called");
+		return "Hello " + text;
+	}
+}

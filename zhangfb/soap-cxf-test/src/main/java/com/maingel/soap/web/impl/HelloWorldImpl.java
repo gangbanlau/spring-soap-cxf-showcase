a63a6b8d1fb19cce0666f5d4862c9ed3b9c4af7b
zhangfb512@gmail.com <zhangfb512@gmail.com@701a5e63-cc8d-c32d-bdff-8dc5e51d885c>
2014-06-24 03:25:12 +0000

    项目完善

--- zhangfb/soap-cxf-test/src/main/java/com/maingel/soap/web/impl/HelloWorldImpl.java
+++ zhangfb/soap-cxf-test/src/main/java/com/maingel/soap/web/impl/HelloWorldImpl.java
@@ -5,7 +5,6 @@ import javax.jws.WebService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.maingel.soap.domain.User;
 import com.maingel.soap.service.IHelloWorldService;

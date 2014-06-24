0e133826ebbb96878fa463beae59d9d0ee85a506
gangban.lau <gangban.lau@701a5e63-cc8d-c32d-bdff-8dc5e51d885c>
2014-06-24 09:18:03 +0000

    remove useless

--- zhangfb/soap-cxf-test/src/test/java/com/maingel/soap/web/HelloWorldImplTest.java
+++ zhangfb/soap-cxf-test/src/test/java/com/maingel/soap/web/HelloWorldImplTest.java
@@ -2,48 +2,30 @@ package com.maingel.soap.web;
 
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.maingel.soap.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(locations={"classpath*:root-context.xml", "classpath*:cxf-context.xml"})
-@TransactionConfiguration
-@Transactional
+@ContextConfiguration(locations={"classpath*:client-beans.xml"})
 public class HelloWorldImplTest {
 	private static final Logger logger = LoggerFactory.getLogger(HelloWorldImplTest.class);
-	
-	private static ApplicationContext context;
+		
+	@Autowired
+	@Qualifier("helloClient")
 	private IHelloWorld helloWorld;
-	
-	@BeforeClass
-	public static void beforeClass() {
-		context = new ClassPathXmlApplicationContext("client-beans.xml");
-	}
-	
-	@AfterClass
-	public static void afterClass() {
-		context = null;
-	}
-
+	  
 	@Test
 	public void testSayHi() {
 		logger.info("Execute testSayHi!");
 		
-		helloWorld = (IHelloWorld)context.getBean("helloClient");
 		assertEquals("Hello World", this.helloWorld.sayHi("World"));
 	}
 
@@ -51,16 +33,13 @@ public class HelloWorldImplTest {
 	public void testFindUserById() {
 		logger.info("Execute testFindUserById!");
 		
-		helloWorld = (IHelloWorld)context.getBean("helloClient");
 		this.helloWorld.findUserById(1);
 	}
 
 	@Test
 	public void testAddUser() {
 		logger.info("Execute testAddUser!");
-		
-		helloWorld = (IHelloWorld)context.getBean("helloClient");
-		
+				
 		User user = new User();
 		user.setName("zhangsan");
 		user.setGender(1);
